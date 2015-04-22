7cd049adc78e46ea11cc1fd2965a719ec374e103
Josh Strom <joshua.strom@intel.com>
2015-04-22 10:52:35 -0700

    Made reading from and writing to source file more reliable. Added initial code to get entry by index and by name

--- sshext.py
+++ sshext.py
@@ -1,10 +1,12 @@
 import sys
 import os
+import collections
 import subprocess
 
 
 def get_full_data_file_name():
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), ".hostslist.txt")
+
 
 def affirmative(answer):
     answer = answer.lower()
@@ -29,25 +31,40 @@ def confirm(message):
 
 
 def load_hosts():
-    pass
+    hosts = collections.OrderedDict()
+    file_name = get_full_data_file_name()
+    if not os.path.isfile(file_name):
+        return hosts
+
+    f = open(file_name, "r")
+    content = f.read().splitlines()
+    for entry in content:
+        nickname, sep, hostname = entry.partition("=")
+        hosts[nickname] = hostname
+
+    f.close()
+    return hosts
 
 
 # Writes the hosts structure to disk. Overwrites file.
-def store_hosts(hosts_structure):
+def store_hosts(hosts):
     f = open(get_full_data_file_name(), "w")
-    for key, value in hosts_structure.items():
-        f.write("{0}={1};".format(key, value))
+    for key, value in hosts.items():
+        f.write("{0}={1}\n".format(key, value))
 
     f.close()
 
 
-def add_host(hostname, nickname):
-    hosts_structure = {hostname: nickname}
-    store_hosts(hosts_structure)
+def get_by_index(hosts, index):
+    return hosts[list(hosts.keys())[index]]
+
+
+def get_by_nickname(hosts, nickname):
+    return hosts[nickname]
 
 
 def register_host():
-
+    hosts = load_hosts()
     hostname = input("Enter host (hostname or IP address): ")
     nickname = input("Enter nickname for entry: ")
     confirmation = confirm("Register host '{0}' with nickname '{1}? (y/n): ".format(hostname, nickname))
@@ -57,17 +74,29 @@ def register_host():
         return
 
     print("Adding '{0}' as '{1}'...".format(hostname, nickname))
-    add_host(hostname, nickname)
+    hosts[nickname] = hostname
+    store_hosts(hosts)
 
 
 def list_hosts():
-    pass
+    hosts = load_hosts()
+    index = 0
+    for nickname, host in hosts.items():
+        print("[{0}] {1}: {2}".format(index, nickname, host))
+        index += 1
+
+
+def connect():
+    hosts = load_hosts()
+    at_index = get_by_index(hosts, 0)
+    print(at_index)
 
 
 def print_help():
     print("Supported commands:")
     print("\t register - Register a host (IP address or hostname) for SSH or SCP.")
     print("\t list - List all registered hosts.")
+    print("\t connect - Connect to a host.")
 
 
 def main():
@@ -82,6 +111,9 @@ def main():
         return
     if command == "list":
         list_hosts()
+        return
+    if command == "connect":
+        connect()
         return
 
     # ... other commands
